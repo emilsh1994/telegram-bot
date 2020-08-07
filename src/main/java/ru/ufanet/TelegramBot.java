c7d478ce386b81a7e8289dfed98509b7128570ac
unknown <emilsh1994@gmail.com>
2020-08-07 19:49:26 +0500

    Добавление работает правильно.

--- src/main/java/ru/ufanet/TelegramBot.java
+++ src/main/java/ru/ufanet/TelegramBot.java
@@ -9,9 +9,14 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TelegramBot extends TelegramLongPollingBot {
+
+    //id пользователя
+    Long user_id;
 
     //Токен выданный BotFather
     public final String botToken = "1258217203:AAGK2sImKKCVSs9oWQp8ivoxBvb0LM7J1Hc";
@@ -29,7 +34,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             "type '/add note-text' - adds a note where 'note-text' is a text of a note,\n" +
             "type '/all' - type this command to see all notes,\n" +
             "type '/del id' - this command deletes specific note by id,\n" +
-            "type '/clear' - this command removes all notes.";
+            "type '/clear' - this command removes all notes.\n";
 
     //Заголовок для списка заметок
     public final String header = "Notes:\n";
@@ -38,16 +43,32 @@ public class TelegramBot extends TelegramLongPollingBot {
     public final String emptyNote = "Note is empty! Type note text after command '/add'.";
 
     //Успешное добавление заметки
-    public final String addedNote = " - Note added successfully! Type '/all' to see all notes.";
+    public final String addedNote = "' - Note added successfully! \n Type '/all' to see all the notes.\n";
 
     //На случай, если еще не было добавлено заметок, но введена команда '/all'
-    public final String emptyNotes = "Sorry, list is empty. To add a note type: '/add' and add a note text after.";
+    public final String emptyNotes = "Sorry, list is empty. To add a note type: '/add' and add a note text after.\n";
 
     //На случай, если еще после команды '/del' ничего не написано
-    public final String emptyDel = "Please, add an id of a note after '/del' command";
+    public final String emptyDel = "Please, add an id of a note after '/del' command\n";
+
+    //Вывод подсказки
+    public final String commandList = "Type '/help' for command list.\n";
+
+    //Очистка заметок прошла успешно.
+    public final String listCleared = "List cleared. Type '/help' for command list.\n";
+
+    //Неправильный id
+    public final String wrongId = "Wrong id. Please try again.\n";
+
+    //Заметка удалена
+    public final String noteDeleted = " deleted successfully!\n";
+
 
     //Список для хранения сообщений
-    List<String> notes = new ArrayList<String>();
+    List<String> notes;
+
+    //Мапа для сохранения пользователя и его листа по id
+    Map<Long, List<String>> map = new HashMap<>();
 
     //Инициализация
     public static void main(String[] args) {
@@ -61,9 +82,9 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     //Метод для отправки сообщений
-    public void sendMessage(Message msg, String text) {
+    public void sendMessage(Long id, String text) {
         SendMessage sendMsg = new SendMessage();
-        sendMsg.setChatId(msg.getChatId());
+        sendMsg.setChatId(id);
         sendMsg.setText(text);
         try {
             execute(sendMsg);
@@ -74,8 +95,20 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     //Listener
     public void onUpdateReceived(Update update) {
+
         //Получаем сообщение в msg
         Message msg = update.getMessage();
+
+        //Получаем id чата
+        user_id = msg.getChatId();
+
+        //Получаем список заметок по id
+        if (!map.containsKey(user_id)) {
+            notes = new ArrayList<>();
+            map.put(user_id, notes);
+        } else {
+            notes = map.get(user_id);
+        }
 
         //Получаем текст из сообщения
         String txt = msg.getText();
@@ -96,9 +129,9 @@ public class TelegramBot extends TelegramLongPollingBot {
 
         if (txt.startsWith("/del")) {
             noteText = txt.replace("/del", "").trim();
-            //Проврка на случай пустого id.
+            //Проверка на случай пустого id.
             if (noteText.length() == 0) {
-                sendMessage(msg, emptyDel);
+                sendMessage(user_id, emptyDel);
                 return;
             } else {
                 note_id = Integer.parseInt(noteText);
@@ -106,38 +139,41 @@ public class TelegramBot extends TelegramLongPollingBot {
             }
         }
 
+        //Выбор команды
         switch (txt) {
+
             //Начало
             case "/start":
-                sendMessage(msg, greeting);
+                sendMessage(user_id, greeting);
                 break;
 
             //Получение списка команд
             case "/help": {
-                sendMessage(msg, help);
+                sendMessage(user_id, help);
                 break;
             }
 
             //Добавление заметки. Пример: '/add Купить хлеб.'
             case "/add": {
+                //Проверям случай, что строка пустая
                 if (noteText.length() == 0) {
-                    sendMessage(msg, emptyNote);
-                } else {
-                    notes.add(noteText);
-                    sendMessage(msg, "'" + noteText + "'" + addedNote);
+                    sendMessage(user_id, emptyNote);
+                    break;
                 }
+                notes.add(noteText);
+                sendMessage(user_id, "Note '" + noteText + addedNote);
                 break;
             }
 
             //Вывод всех заметок
             case "/all": {
                 if (notes.size() == 0) {
-                    sendMessage(msg, emptyNotes);
+                    sendMessage(user_id, emptyNotes);
                 } else {
                     for (int i = 0; i < notes.size(); i++) {
                         s += "#" + (i + 1) + " " + notes.get(i) + "\n";
                     }
-                    sendMessage(msg, header + s + "Type '/help' for command list.\n");
+                    sendMessage(user_id, header + s + commandList);
                 }
                 break;
             }
@@ -145,18 +181,18 @@ public class TelegramBot extends TelegramLongPollingBot {
             //Удаление заметки из списка. Пример: '/del 2'
             case "/del": {
                 if (note_id < 1 || note_id > notes.size()) {
-                    sendMessage(msg, "Wrong id. Please try again.");
+                    sendMessage(user_id, wrongId);
                 } else {
-                    String removed = notes.get(note_id - 1);
                     notes.remove(note_id - 1);
-                    sendMessage(msg, removed + " - Note removed successfully!");
+                    sendMessage(user_id, "#" + note_id + noteDeleted);
                 }
+                break;
             }
 
             //Удалить все заметки
             case "/clear": {
                 notes.removeAll(notes);
-                sendMessage(msg, "List cleared. Type '/help' for command list.\n");
+                sendMessage(user_id, listCleared);
             }
         }
     }
